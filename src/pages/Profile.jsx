6ce239577dbdd54161bebd31625a411d26b06ed5
Arthur Mendes <arthurmendes.fatec@gmail.com>
2024-10-05 14:38:57 -0300

    feat: finish profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,8 +3,27 @@ import { TopMenu } from "@/components/TopMenu"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 function Profile() {
+    const [notifications, setNotifications] = useState([
+        {name: "Nome da notificação", datetime: "16:22", description: "Descrição da notificação", visualized: true},
+        {name: "Nome da notificação", datetime: "18:50", description: "Descrição da notificação", visualized: false},
+        {name: "Nome da notificação", datetime: "13:00", description: "Descrição da notificação", visualized: false},
+        {name: "Nome da notificação", datetime: "17:50", description: "Descrição da notificação", visualized: true},
+    ])
+    const [blockEdit, setBlockEdit] = useState(true)
+    const [inputs, setInputs] = useState({email: "email.usuario@gmai.com", password: "senhadousuario"})
+
+    function handleEditProfile() {
+        if(!blockEdit) {
+            alert(`Email: ${inputs.email} \nSenha: ${inputs.password}`)
+        }
+
+        setBlockEdit(!blockEdit)
+    }
+
     return (
         <>
             <TopMenu />
@@ -16,9 +35,9 @@ function Profile() {
                     <div className='flex gap-4 flex-wrap justify-center w-full min-h-300px bg-wise-hyper_black rounded-lg p-4'>
                         <img src='/default_profile_image.jpg' alt="Foto de perfil" className="max-w-[150px] flex-1 rounded-md object-cover border-none" />
 
-                        <div className="border border-red-500 flex-1 flex justify-between gap-5 flex-wrap montserrat">
+                        <div className="flex-1 flex justify-between gap-5 flex-wrap montserrat">
                             <div className='text-wise-light_white'>
-                                <h2 className="font-semibold text-2xl">Nome do usuário</h2>
+                                <h2 className="font-semibold text-4xl">Nome do usuário</h2>
                                 <p><span className="text-base text-wise-hyper_light_green">Cargo</span> | Empresa do usuário</p>
                             </div>
 
@@ -26,22 +45,24 @@ function Profile() {
                                 <Label className='text-wise-light_white flex flex-col gap-2'>
                                     Email
 
-                                    <Input disabled value='email.usuario@gmai.com' className='w-full max-w-[300px] min-w-[250px] text-wise-hyper_light_green bg-transparent opacity-100' />
+                                    <Input disabled={blockEdit} value={inputs.email} onChange={(ev) => {setInputs({...inputs, email: ev.target.value})}} className='w-full max-w-[300px] min-w-[250px] text-wise-hyper_light_green bg-transparent opacity-100' />
                                 </Label>
 
                                 <Label className='text-wise-light_white flex flex-col gap-2'>
                                     Senha
 
-                                    <Input disabled value='senhadousuario' className='w-full max-w-[300px] min-w-[250px] text-wise-hyper_light_green bg-transparent opacity-100' />
+                                    <Input disabled={blockEdit} value={inputs.password} onChange={(ev) => {setInputs({...inputs, password: ev.target.value})}} className='w-full max-w-[300px] min-w-[250px] text-wise-hyper_light_green bg-transparent opacity-100' />
                                 </Label>
                             </div>
 
-                            <div className="w-full flex justify-between items-end text-wise-light_white">
-                                <small>No sistema desde: <span className="text-wise-hyper_light_green">12/05/2024</span></small>
+                            <div className="w-full flex flex-wrap justify-between items-end text-wise-light_white gap-3">
+                                <h4 className="text-xl">No sistema desde: <span className="text-wise-hyper_light_green">12/05/2024</span></h4>
 
                                 <div className="flex gap-2 items-center">
-                                    <Button variant='outline'>Sair do sistema</Button>
-                                    <Button >Editar informações</Button>
+                                    <Link to='/logout'><Button variant='outline' className="text-wise-hyper_black">Sair do sistema</Button></Link>
+                                    <Button onClick={handleEditProfile}>
+                                        {blockEdit ? "Editar informações" : "Salvar alterações"}
+                                    </Button>
                                 </div>
                             </div>
                         </div>
@@ -53,23 +74,22 @@ function Profile() {
 
                     <div className=''>
                         <ul>
-                            <li className="flex justify-between flex-wrap items-center gap-3 py-3 border-b border-b-wise-dark_green last:border-none">
-                                <div>
-                                    <h2 className="font-semibold">Nome da notificação - HH:MM</h2>
-                                    <p className="text-sm">Descrição da notificação</p>
-                                </div>
+                            {
+                                notifications.map((notification, idx) => {
+                                    return (
+                                        <li id={idx} className={`${notification.visualized && 'opacity-55'} flex justify-between flex-wrap items-center gap-3 py-3 border-b border-b-wise-dark_green last:border-none`}>
+                                            <div>
+                                                <h2 className="font-semibold">{notification.name} - {notification.datetime}</h2>
+                                                <p className="text-sm">{notification.description}</p>
+                                            </div>
 
-                                <Button variant='outline' className="border-wise-dark_green text-wise-dark_green">Marcar como visualizada</Button>
-                            </li>
-
-                            <li className="flex justify-between flex-wrap items-center gap-3 py-3 border-b border-b-wise-dark_green last:border-none">
-                                <div>
-                                    <h2 className="font-semibold">Nome da notificação - HH:MM</h2>
-                                    <p className="text-sm">Descrição da notificação</p>
-                                </div>
-
-                                <Button variant='outline' className="border-wise-dark_green text-wise-dark_green">Marcar como visualizada</Button>
-                            </li>
+                                            <Button variant='outline' className={notification.visualized ? 'border-wise-dark_green text-wise-dark_green' : 'border-wise-dark_green bg-wise-dark_green text-wise-light_white'}>
+                                                {notification.visualized ? "Visualizado" : "Marcar como visualizada"}
+                                            </Button>
+                                        </li>
+                                    )
+                                })
+                            }
                         </ul>
                     </div>
                 </section>

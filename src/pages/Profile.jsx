206b82f88c8d8c8000581036915059a318dde94c
NicoleCarvalh <nicolecarvalho6@gmail.com>
2024-11-25 18:12:51 -0300

    Fixing profile page design on mobile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -40,7 +40,7 @@ function Profile() {
         <section className="flex flex-col gap-8">
           <h1 className="text-2xl font-bold">Perfil</h1>
 
-          <div className="flex gap-4 flex-wrap justify-center items-center w-full min-h-300px bg-wise-hyper_black rounded-lg p-4">
+          <div className="flex gap-4 flex-wrap justify-center items-center w-full min-h-300px bg-wise-hyper_black rounded-lg p-4 mb-1">
             <img
               src={
                 currentUser && currentUser?.photoUrl
@@ -48,10 +48,10 @@ function Profile() {
                   : "/default_profile_image.jpg"
               }
               alt="Foto de perfil"
-              className="max-w-[350px] min-h-[250px] flex-1 rounded-md object-cover border-none"
+              className="max-w-[300px] md:max-w-[350px] min-h-[250px] flex-1 rounded-md object-cover border-none"
             />
 
-            <div className="flex-1 flex justify-between gap-5 flex-wrap montserrat">
+            <div className="flex-1 flex justify-center md:justify-between gap-5 flex-wrap montserrat">
               <div className="text-wise-light_white">
                 <h2 className="font-semibold text-5xl">{currentUser?.name}</h2>
                 <p>
@@ -89,7 +89,7 @@ function Profile() {
                 </Label>
               </div>
 
-              <div className="w-full flex flex-wrap justify-between items-end text-wise-light_white gap-3">
+              <div className="w-full flex flex-wrap justify-center md:justify-between items-end text-wise-light_white gap-3">
                 <h4 className="text-xl">
                   No sistema desde:{" "}
                   <span className="text-wise-hyper_light_green">
@@ -111,31 +111,6 @@ function Profile() {
             </div>
           </div>
         </section>
-        {/* 
-                <section className="flex flex-col gap-8">
-                    <h1 className='text-2xl font-bold'>Últimas notificações</h1>
-
-                    <div className=''>
-                        <ul>
-                            {
-                                notifications.map((notification, idx) => {
-                                    return (
-                                        <li key={idx} className={`${notification.visualized && 'opacity-55'} flex justify-between flex-wrap items-center gap-3 py-3 border-b border-b-wise-dark_green last:border-none`}>
-                                            <div>
-                                                <h2 className="font-semibold">{notification.name} - {notification.datetime}</h2>
-                                                <p className="text-sm">{notification.description}</p>
-                                            </div>
-
-                                            <Button variant='outline' className={notification.visualized ? 'border-wise-dark_green text-wise-dark_green' : 'border-wise-dark_green bg-wise-dark_green text-wise-light_white'}>
-                                                {notification.visualized ? "Visualizado" : "Marcar como visualizada"}
-                                            </Button>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </div>
-                </section> */}
         <ChatTab />
       </MainContainer>
     </>
